a0ec01c7e63ea59d70f9ea858e13f1123474a1b4
adimaryadi <adi.maryadi@dwidasa.com>
2022-03-25 13:35:54 +0700

    first build

---
+++ src/pages/HomePage.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import { Route, Switch, Redirect } from "react-router-dom";
+import { Routes } from "../routes";
+
+// pages
+import DashboardOverview from "./dashboard/DashboardOverview";
+import Transactions from "./Transactions";
+// components
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Preloader from "../components/Preloader";
+
+// eslint-disable-next-line
+const RouteWithLoader = ({ component: Component, ...rest }) => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoaded(true), 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <Route {...rest} render={props => ( <> <Preloader show={loaded ? false : true} /> <Component {...props} /> </> ) } />
+  );
+};
+
+const RouteWithSidebar = ({ component: Component, ...rest }) => {
+  // eslint-disable-next-line
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoaded(true), 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const localStorageIsSettingsVisible = () => {
+    return localStorage.getItem('settingsVisible') === 'false' ? false : true
+  }
+
+  const [showSettings, setShowSettings] = useState(localStorageIsSettingsVisible);
+
+  const toggleSettings = () => {
+    setShowSettings(!showSettings);
+    localStorage.setItem('settingsVisible', !showSettings);
+  }
+
+  return (
+    <Route {...rest} render={props => (
+      <>
+        {/* <Preloader show={loaded ? false : true} /> */}
+        <Sidebar />
+
+        <main className="content">
+          <Navbar />
+          <Component {...props} />
+          <Footer toggleSettings={toggleSettings} showSettings={showSettings} />
+        </main>
+      </>
+    )}
+    />
+  );
+};
+
+export default () => (
+  <Switch>
+    <RouteWithSidebar exact path={Routes.DashboardOverview.path} component={DashboardOverview} />
+    <RouteWithSidebar exact path={Routes.Transactions.path} component={Transactions} />
+    <Redirect to={Routes.NotFound.path} />
+  </Switch>
+);
